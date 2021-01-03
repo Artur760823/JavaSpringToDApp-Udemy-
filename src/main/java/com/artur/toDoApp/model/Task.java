f7380dfa3abe17e89f79c0c67915481997705045
Artur <artur.zajaczkowski76@gmail.com>
2021-01-03 18:08:45 +0100

    Add validation to description; massage desc must not be null + add configValid in ToDoAppApplication.java

--- src/main/java/com/artur/toDoApp/model/Task.java
+++ src/main/java/com/artur/toDoApp/model/Task.java
@@ -1,15 +1,21 @@
 package com.artur.toDoApp.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity //under this will be table from DB (every variable will be collumn in table)
 @Table(name = "tasks")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) //strategy of generate id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //strategy of generate id
     private int id; //id needs @Id
+
+    @NotBlank(message="Tasks description must be not null") //validation description
     private String description;
     private boolean done;
+
+    public Task() {
+    }
 
     public int getId() {
         return id;
