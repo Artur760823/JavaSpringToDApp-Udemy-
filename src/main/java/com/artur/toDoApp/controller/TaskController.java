76baebde6d21f9d9aac3396bb90b19567b0dc89b
Artur <artur.zajaczkowski76@gmail.com>
2021-01-05 10:17:23 +0100

    Clearing & optimalizing RestRepo

--- src/main/java/com/artur/toDoApp/controller/TaskController.java
+++ src/main/java/com/artur/toDoApp/controller/TaskController.java
@@ -1,13 +1,17 @@
 package com.artur.toDoApp.controller;
 
+import com.artur.toDoApp.model.Task;
 import com.artur.toDoApp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RepositoryRestController
+import java.util.List;
+
+@RestController
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -16,10 +20,15 @@ public class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
-    ResponseEntity<?> readAllTasks(){
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        logger.info("Custom pager");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
 }
